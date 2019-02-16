6d9c7700aae3459facc3a1b0f4d5a4896da449d6
Babita <babitav2003@yahoo.co.in>
2019-02-16 21:56:44 +0000

    First version of sumupto.py Program 1 of problem set

---
+++ sumupto.py
@@ -0,0 +1,13 @@
+# Babita, 2019-01-30
+# Sum of numbers from 1 to the input number 
+
+sum = 0
+
+i = int(input("Please enter a positive number less than 10 "))
+
+while i < 11:
+  sum = sum + i
+  i = i + 1
+
+print ("The sum of numbers is", sum)
+
