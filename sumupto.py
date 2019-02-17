450160ea093b9ff87eacd782f609d84bc205008a
Babita <babitav2003@yahoo.co.in>
2019-02-17 12:53:39 +0000

    Final version of sumupto.py

--- sumupto.py
+++ sumupto.py
@@ -3,19 +3,19 @@
 
 sum = 0
 i = 1
-sumupto = int(input("Please enter a positive number less than 10 "))
+sumupto = int(input("Please enter a positive number"))
 
+# Check if the input number is not a positive integer(less than 1), ask user to enter the number again
 while True:
-  if sumupto >10 :
-    sumupto = int(input("Please enter a positive number less than 10 "))
-  elif sumupto <=0 : 
-      sumupto = int(input("Please enter a positive number less than 10 "))
+  if sumupto <=0 : 
+      sumupto = int(input("Please enter a positive number"))
   else:
     break
 
+# Loop from 1 till the input number and add the number i to the sum
 while i <= sumupto :
   sum = sum + i
   i = i + 1
 
-print ("The sum of numbers is", sum)
+print ("The sum of numbers from 1 to",sumupto, "is ", sum)
 
