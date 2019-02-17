e6c0ddb1d5c8a4edae3c6a9b84d46273be837eea
Babita <babitav2003@yahoo.co.in>
2019-02-17 01:09:25 +0000

    Input validations and corrections to sumupto.py

--- sumupto.py
+++ sumupto.py
@@ -2,10 +2,18 @@
 # Sum of numbers from 1 to the input number 
 
 sum = 0
+i = 1
+sumupto = int(input("Please enter a positive number less than 10 "))
 
-i = int(input("Please enter a positive number less than 10 "))
+while True:
+  if sumupto >10 :
+    sumupto = int(input("Please enter a positive number less than 10 "))
+  elif sumupto <=0 : 
+      sumupto = int(input("Please enter a positive number less than 10 "))
+  else:
+    break
 
-while i < 11:
+while i <= sumupto :
   sum = sum + i
   i = i + 1
 
